a6a54fc2ab55659c7ee1a13532cec6cabf82b8a0
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-04-22 22:53:38 -0300

    [Config] Exchanged hardcoded firebase config initialization for one fetched using the CLI

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,17 +2,8 @@ import app from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
-import config from "../config/firebase.config";
-
-const firebase = app.initializeApp({
-  apiKey: "AIzaSyAAHadVAUzTBODDFScQwbupxM8RqWzQt_c",
-  authDomain: "munay-nextjs-hefesto.firebaseapp.com",
-  projectId: "munay-nextjs-hefesto",
-  storageBucket: "munay-nextjs-hefesto.appspot.com",
-  messagingSenderId: "935545228005",
-  appId: "1:935545228005:web:cae2d01abd175776ffa20c",
-  measurementId: "G-Q6GGYPRL0K",
-});
+const firebaseConfig = require("../../firebase-config.json").results.sdkConfig;
+const firebase = app.initializeApp(firebaseConfig);
 
 export const db = firebase.firestore();
 export const auth = firebase.auth();
